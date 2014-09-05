66178b0023a9e15e64feb2e64ecf13ba6b563ca5
Phil Bayfield <phil@bayfmail.com>
2014-09-05 19:13:59 +0100

    animate transitions

--- js/theme.js
+++ js/theme.js
@@ -4,15 +4,24 @@ $(document).ready(function(){
         $('#btn-list').removeClass('active');
         $('#content').find('.col-outer').removeClass('col-lg-12').addClass('col-lg-3 col-md-4 col-sm-6 col-xs-12');
         $('#content').removeClass('content-list').addClass('content-grid');
-        $('#content').find('.col-inner').height('auto').equalHeights();
         $('#content').height('auto');
+        $('#content').find('.col-inner').height('auto').addEventListener(
+            'webkitTransitionEnd',
+            function(event) {
+                $('#content').find('.col-inner').equalHeights();
+            }, false );
     });
     $('#btn-list').click(function() {
         $('#btn-list').addClass('active');
         $('#btn-grid').removeClass('active');
         $('#content').find('.col-outer').removeClass('col-lg-3 col-md-4 col-sm-6 col-xs-12').addClass('col-lg-12');
         $('#content').removeClass('content-grid').addClass('content-list').equalHeights();
-        $('#content').find('.col-inner').height('auto').equalHeights();
         $('#content').height('auto');
+        $('#content').find('.col-inner').height('auto').addEventListener(
+            'webkitTransitionEnd',
+            function(event) {
+                $('#content').find('.col-inner').equalHeights();
+            }, false );
+
     });
 });
