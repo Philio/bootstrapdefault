1ef348579f502b2a02cc518e5876b7be3408cb6f
Phil Bayfield <phil@bayfmail.com>
2014-09-05 19:34:13 +0100

    fix js error and reorganise the js more efficiently

--- js/theme.js
+++ js/theme.js
@@ -2,26 +2,37 @@ $(document).ready(function(){
     $('#btn-grid').click(function() {
         $('#btn-grid').addClass('active');
         $('#btn-list').removeClass('active');
-        $('#content').find('.col-outer').removeClass('col-lg-12').addClass('col-lg-3 col-md-4 col-sm-6 col-xs-12');
-        $('#content').removeClass('content-list').addClass('content-grid');
-        $('#content').height('auto');
-        $('#content').find('.col-inner').height('auto').addEventListener(
-            'webkitTransitionEnd',
-            function() {
-                $('#content').find('.col-inner').height('auto').equalHeights();
-            }, false );
+        $('#content')
+            .removeClass('content-list')
+            .addClass('content-grid')
+            .height('auto')
+            .find('.col-outer')
+                .removeClass('col-lg-12')
+                .addClass('col-lg-3 col-md-4 col-sm-6 col-xs-12')
+                .one(
+                    'webkitTransitionEnd',
+                    function() {
+                        $('#content').find('.col-inner').equalHeights();
+                    })
+            .find('.col-inner')
+                .height('auto');
     });
     $('#btn-list').click(function() {
         $('#btn-list').addClass('active');
         $('#btn-grid').removeClass('active');
-        $('#content').find('.col-outer').removeClass('col-lg-3 col-md-4 col-sm-6 col-xs-12').addClass('col-lg-12');
-        $('#content').removeClass('content-grid').addClass('content-list').equalHeights();
-        $('#content').height('auto');
-        $('#content').find('.col-inner').height('auto').addEventListener(
-            'webkitTransitionEnd',
-            function() {
-                $('#content').find('.col-inner').height('auto').equalHeights();
-            }, false );
-
+        $('#content')
+            .removeClass('content-grid')
+            .addClass('content-list')
+            .height('auto')
+            .find('.col-outer')
+                .removeClass('col-lg-3 col-md-4 col-sm-6 col-xs-12')
+                .addClass('col-lg-12')
+                .one(
+                    'webkitTransitionEnd',
+                    function() {
+                        $('#content').find('.col-inner').equalHeights();
+                    })
+            .find('.col-inner')
+                .height('auto');
     });
 });
