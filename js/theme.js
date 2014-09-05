f0de744134fd2b06ec075e0a258d9b31ee217050
Phil Bayfield <phil@bayfmail.com>
2014-09-05 17:20:03 +0100

    work in progress for switching view to test on live gallery

--- js/theme.js
+++ js/theme.js
@@ -4,13 +4,13 @@ $(document).ready(function(){
         $('#btn-list').removeClass('active');
         $('#content').find('.col-outer').removeClass('col-lg-12').addClass('col-lg-3 col-md-4 col-sm-6 col-xs-12');
         $('#content').removeClass('content-list').addClass('content-grid');
-        $('#content .col-inner').equalHeights();
+        $('#content .col-inner').height('auto').equalHeights();
     });
     $('#btn-list').click(function() {
         $('#btn-list').addClass('active');
         $('#btn-grid').removeClass('active');
         $('#content').find('.col-outer').removeClass('col-lg-3 col-md-4 col-sm-6 col-xs-12').addClass('col-lg-12');
         $('#content').removeClass('content-grid').addClass('content-list').equalHeights();
-        $('#content .col-inner').equalHeights();
+        $('#content .col-inner').height('auto').equalHeights();
     });
 });
