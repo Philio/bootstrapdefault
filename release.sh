70defe7c199935e6328e61a978394ab9e9ecdfed
Phil Bayfield <phil@bayfmail.com>
2015-05-25 22:26:37 +0100

    add release script

---
+++ release.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+if [ $# -eq 0 ]
+  then
+    echo "Usage: release.sh <version>"
+fi
+mkdir -p $1/bootstrapdefault
+mkdir -p $1/bootstrapdefault/bootstrap/dist
+mkdir -p $1/bootstrapdefault/selectize.js/dist
+cp -rv bootstrap/dist/* $1/bootstrapdefault/bootstrap/dist
+cp -rv selectize.js/dist/* $1/bootstrapdefault/selectize.js/dist
+cp -rv README.md fixplugins.css img js less local_head.tpl template theme.css themeconf.inc.php $1/bootstrapdefault/
+cd $1
+zip -r ../bootstrapdefault-$1.zip bootstrapdefault
+cd ..
+rm -r $1
