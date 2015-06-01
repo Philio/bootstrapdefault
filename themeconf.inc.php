a3bcbf15ce745aa97b14c26e9369b25494eb2ed1
Phil Bayfield <phil@bayfmail.com>
2015-06-01 07:55:24 +0100

    Update theme config

--- themeconf.inc.php
+++ themeconf.inc.php
@@ -1,10 +1,10 @@
 <?php
 /*
 Theme Name: Bootstrap Default
-Version: 0.0.1
-Description: Bootstrap Default theme for Piwigo
-Theme URI:
-Author: Phil Bayfield
+Version: 0.9.9-pr2
+Description: A modern and responsive theme for Piwigo built with standard Bootstrap components and using the default Bootstrap theme. Intended for easy customisation using Bootstrap styles or as a parent theme for creating responsive themes for Piwigo.
+Theme URI: http://piwigo.org/ext/extension_view.php?eid=796
+Author: Phil Bayfield (philb)
 Author URI: https://philio.me/
 */
 $themeconf = array(
