f843c82c03c260320cd88917012ba8123d967415
Phil Bayfield <phil@bayfmail.com>
2015-06-01 08:28:36 +0100

    update release script

--- release.sh
+++ release.sh
@@ -8,7 +8,7 @@ mkdir -p $1/bootstrapdefault/bootstrap/dist
 mkdir -p $1/bootstrapdefault/selectize.js/dist
 cp -rv bootstrap/dist/* $1/bootstrapdefault/bootstrap/dist
 cp -rv selectize.js/dist/* $1/bootstrapdefault/selectize.js/dist
-cp -rv fixplugins.css img js less local_head.tpl screenshot.png template theme.css themeconf.inc.php $1/bootstrapdefault/
+cp -rv fixplugins.css img index.php js less local_head.tpl screenshot.png template theme.css themeconf.inc.php $1/bootstrapdefault/
 cd $1
 zip -r ../bootstrapdefault-$1.zip bootstrapdefault
 cd ..
